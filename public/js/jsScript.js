6f2ee81ea1c01c62511be4435c1edfb6d64aa62e
Newaz <newazbenalam@gmail.com>
2022-08-08 22:18:44 +0600

    graph data

--- public/js/jsScript.js
+++ public/js/jsScript.js
@@ -4,48 +4,49 @@ element = document.getElementsByTagName("nav")[0];
 let lastKnownScrollPosition = 0;
 
 function func(x, y) {
-    //console.log(x + " " + y);
-    if (x < y) {
-        if (element.style.top != "0px") {
-            element.style.top = "0px";
-        }
-    } else {
-        if (window.screen.width < window.screen.height * .65) {
-            element.style.top = "-52px";
-        }
-        lastKnownScrollPosition = window.scrollY;
+  //console.log(x + " " + y);
+  if (x < y) {
+    if (element.style.top != "0px") {
+      element.style.top = "0px";
     }
+  } else {
+    if (window.screen.width < window.screen.height * 0.65) {
+      element.style.top = "-52px";
+    }
+    lastKnownScrollPosition = window.scrollY;
+  }
 }
 
-document.addEventListener("scroll", () =>
-    func(window.scrollY, lastKnownScrollPosition),
-    setTimeout(() => 100)
+document.addEventListener(
+  "scroll",
+  () => func(window.scrollY, lastKnownScrollPosition),
+  setTimeout(() => 100)
 );
 
 setTimeout(() => {
-    if (localStorage.getItem("theme") == "true") {
-        document.documentElement.classList.toggle("dark");
-        console.log(localStorage.getItem("theme"));
-    }
-    console.log(localStorage.getItem("theme"));
-}, 0)
-
-function dark_mode(){
-    if (localStorage.getItem("theme") == "true") {
-        localStorage.setItem("theme", false);
-    } else {
-        localStorage.setItem("theme", true);
-    }
+  if (localStorage.getItem("theme") == "true") {
     document.documentElement.classList.toggle("dark");
     console.log(localStorage.getItem("theme"));
+  }
+  console.log(localStorage.getItem("theme"));
+}, 0);
+
+function dark_mode() {
+  if (localStorage.getItem("theme") == "true") {
+    localStorage.setItem("theme", false);
+  } else {
+    localStorage.setItem("theme", true);
+  }
+  document.documentElement.classList.toggle("dark");
+  console.log(localStorage.getItem("theme"));
 }
 
 document.querySelector("#dark-mode-toggle").onclick = () => {
-    dark_mode();
+  dark_mode();
 };
 
 function loader() {
-    document.querySelector(".loader-container").classList.add("active");
+  document.querySelector(".loader-container").classList.add("active");
 }
 
 /*document.querySelector("#dark-mode-toggle-mb").onclick = () => {
@@ -53,18 +54,34 @@ function loader() {
 };*/
 
 var trace1 = {
-    type: 'bar',
-    x: [1, 2, 3, 4],
-    y: [11, 10, 5, 8],
-    marker: {
-        color: '#C8A2C8',
-        line: {
-            width: 2.5
-        }
-    }
+  type: "bar",
+  x: [1, 2, 3, 4],
+  y: [11, 10, 5, 8],
+  marker: {
+    color: "#C8A2C8",
+    line: {
+      width: 2.5,
+    },
+  },
+};
+
+function bar() {
+  const Http = new XMLHttpRequest();
+  const url = "/bar";
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
   };
-  
-  Plotly.newPlot('bar1', [ trace1 ], { 
-    title: 'Nice Bar Graph',
-    font: {size: 18}
-  }, {responsive: true, theme: 'dark'} );
+}
+
+Plotly.newPlot(
+  "bar1",
+  [trace1],
+  {
+    title: "Nice Bar Graph",
+    font: { size: 18 },
+  },
+  { responsive: true, theme: "dark" }
+);
--- server/models/graph.js
+++ server/models/graph.js
@@ -2,15 +2,19 @@ var connection = require('../controllers/mysql');
 
 let tbname = 'epa_daily_t'
 let sql = `SELECT mean, Daily FROM ${tbname}`
+let arrX = [], arrY = [], arr2D = [];
 
 exports.bar = async(req, res) => {
   connection.query(sql, function (error, results) {
     if (error) throw error;
-    // console.log(results)
-    
     
 
-    arr = Object.values(results[0])
-    res.send(arr);
+    for (let i = 0; i < results.length; i++) {
+      arrX[i] = Object.values(results[i])[0]; 
+      arrY[i] = Object.values(results[i])[1]; 
+    }
+    arr2D = [arrY, arrX];
+    // arr = Object.values(results[0])
+    res.send(arr2D);
   });
 }
