3295ce570f2dcee132b2eb2781b31fb1a81a036d
Newaz <newazbenalam@gmail.com>
2022-08-14 01:49:18 +0600

    aqiCard dynamic color

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -12,9 +12,34 @@ exports.search = async(req, res) => {
         try{
             if(err) throw err;
             console.log(result)
-            let aqiCard = result 
-            console.log(aqiCard)           
-            res.render("home", {aqiCard})
+            let aqiCard = result;
+            let color = "green"; 
+            console.log(aqiCard)         
+            switch (true) {
+                case (aqiCard[0].PM25 < 50):
+                    color = "green";
+                    break;
+                case (aqiCard[0].PM25 < 100):
+                    color = "yellow";
+                    break;
+                case (aqiCard[0].PM25 < 150):
+                    color = "orange";
+                    break;
+                case (aqiCard[0].PM25 < 200):
+                    color = "crimson";
+                    break;
+                case (aqiCard[0].PM25 < 300):
+                    color = "purple";
+                    break;
+                case (aqiCard[0].PM25 < 300):
+                    color = "brown";
+                    break;
+                default:
+                    color = "rgb(0,128,0)";
+                    break;
+            }
+            console.log(aqiCard[0].PM25)
+            res.render("home", {aqiCard, color})
         }
         catch(err){
             console.log(err)
