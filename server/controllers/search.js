45c887a9df672fceac01a80b8b110a508e3a9ac9
Newaz <newazbenalam@gmail.com>
2022-08-14 02:01:19 +0600

    Color fix

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -14,32 +14,39 @@ exports.search = async(req, res) => {
             console.log(result)
             let aqiCard = result;
             let color = "green"; 
+            let level = "Good";
             console.log(aqiCard)         
             switch (true) {
                 case (aqiCard[0].PM25 < 50):
                     color = "green";
+                    level = "Good";
                     break;
                 case (aqiCard[0].PM25 < 100):
                     color = "yellow";
+                    level = "Moderate";
                     break;
                 case (aqiCard[0].PM25 < 150):
                     color = "orange";
+                    level = "Unhealty Sensitive";
                     break;
                 case (aqiCard[0].PM25 < 200):
                     color = "crimson";
+                    level = "Unhealthy";
                     break;
                 case (aqiCard[0].PM25 < 300):
                     color = "purple";
+                    level = "Very Unhealthy";
                     break;
                 case (aqiCard[0].PM25 < 300):
                     color = "brown";
+                    level = "Hazardous";
                     break;
                 default:
                     color = "rgb(0,128,0)";
                     break;
             }
             console.log(aqiCard[0].PM25)
-            res.render("home", {aqiCard, color})
+            res.render("home", {aqiCard, color, level})
         }
         catch(err){
             console.log(err)
